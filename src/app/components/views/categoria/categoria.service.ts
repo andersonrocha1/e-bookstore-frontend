08bf2ed3d438439a7fa9143ea1cb6ae8b80174c7
andersonrocha1 <anderson.tec.informacao@gmail.com>
2023-02-13 22:43:03 -0300

    Cadastrando Categoria com método Create

--- src/app/components/views/categoria/categoria.service.ts
+++ src/app/components/views/categoria/categoria.service.ts
@@ -1,5 +1,7 @@
+
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { Categoria } from './categoria.model';
@@ -11,10 +13,24 @@ export class CategoriaService {
 
   baseUrl: String = environment.BASE_URL;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private _snack: MatSnackBar) { }
 
   findAll():Observable<Categoria[]>{
   const url = `${this.baseUrl}categorias`
     return this.http.get<Categoria[]>(url)
+  }
+
+  create(categoria: Categoria):Observable<Categoria> {
+    const url = `${this.baseUrl}categorias`
+    return this.http.post<Categoria>(url, categoria);
+
+  }
+
+  mensagem(msg: String): void{
+      this._snack.open(`${msg}`, 'OK',{
+        horizontalPosition: 'end',
+        verticalPosition: 'top',
+        duration: 3000
+      })
   }
 }
