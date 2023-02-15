3ee1e81562623022c5f030cd84a605873c83741d
andersonrocha1 <anderson.tec.informacao@gmail.com>
2023-02-15 13:17:55 -0300

    Iniciando views de livros(html e css)

---
+++ src/app/components/views/livro/livro-read-all/livro-read-all.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-livro-read-all',
+  templateUrl: './livro-read-all.component.html',
+  styleUrls: ['./livro-read-all.component.css']
+})
+export class LivroReadAllComponent {
+
+  displayedColumns: string[] = ['id', 'titulo', 'livros', 'acoes'];
+
+}
